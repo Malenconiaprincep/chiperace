67ce198d31ba77c7dc485f5ff37f2121d0f0004a
bo.wang <bo.wang@eeoa.com>
2025-03-27 19:56:02 +0800

    fix: 🐛 title

--- web/docusaurus.config.ts
+++ web/docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'CHI PIERCE',
+  title: '广东芯培森技术有限公司',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
